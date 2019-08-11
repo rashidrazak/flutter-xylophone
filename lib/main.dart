0f1a0e5a2a901ca541dfa8c782c5fde34f725735
Rashid Razak <bib.nexus@gmail.com>
2019-08-12 03:17:46 +0800

    Finish UI and layout.

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,45 @@ class _XylophoneAppState extends State<XylophoneApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        appBar: AppBar(
+          title: Text('Flutter Xylophone'),
+          backgroundColor: Colors.blueGrey.shade900,
+        ),
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                    color: Colors.red, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.orange, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.yellow, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.green, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.teal, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.blue, onPressed: () {}, child: null),
+              ),
+              Expanded(
+                child: FlatButton(
+                    color: Colors.purple, onPressed: () {}, child: null),
+              ),
+            ],
+          ),
         ),
       ),
     );
